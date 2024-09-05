c8d0252c4ea12d690e59eba171683fdb7e0324f5
Rudi Setiyawan <diphons@gmail.com>
2024-09-05 15:31:30 +0700

    Add Support D8G Feature
    - OProfile
    - Game Ai
    - Dynamic Charging
    - Thermal Zone
    - Skip Thermal
    - Oplus Panel Status
    - Refreshrate Status
    - Set Amoled Display
    - Haptic Level
    Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -9,6 +9,9 @@
 #include <linux/gpio.h>
 #include <linux/of_gpio.h>
 #include <linux/pwm.h>
+#ifdef CONFIG_D8G_SERVICE
+#include <misc/d8g_helper.h>
+#endif
 #include <video/mipi_display.h>
 
 #include "dsi_panel.h"
@@ -38,6 +41,9 @@
 
 extern struct frame_stat fm_stat;
 extern void sde_crtc_fod_ui_ready(struct dsi_display *display, int type, int value);
+#ifdef CONFIG_D8G_SERVICE
+static int oprofile_last = -1; // get last profile by oprofile
+#endif
 
 enum dsi_dsc_ratio_type {
 	DSC_8BPC_8BPP,
@@ -4524,6 +4530,11 @@ int dsi_panel_set_lp1(struct dsi_panel *panel)
 		panel->power_mode != SDE_MODE_DPMS_LP2)
 		dsi_pwr_panel_regulator_mode_set(&panel->power_info,
 			"ibb", REGULATOR_MODE_IDLE);
+#ifdef CONFIG_D8G_SERVICE
+	oplus_panel_status = 3; // DISPLAY_POWER_DOZE
+	oprofile_last = oprofile; // Get latest profile
+	oprofile = 4; // set oprofile to battery when display off
+#endif
 	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_LP1);
 	if (rc)
 		DSI_ERR("[%s] failed to send DSI_CMD_SET_LP1 cmd, rc=%d\n",
@@ -4551,6 +4562,11 @@ int dsi_panel_set_lp2(struct dsi_panel *panel)
 	if (!panel->panel_initialized)
 		goto exit;
 
+#ifdef CONFIG_D8G_SERVICE
+	oplus_panel_status = 4; // DISPLAY_POWER_DOZE_SUSPEND
+	oprofile_last = oprofile; // Get latest profile
+	oprofile = 4; // set oprofile to battery when display off
+#endif
 	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_LP2);
 	if (rc)
 		DSI_ERR("[%s] failed to send DSI_CMD_SET_LP2 cmd, rc=%d\n",
@@ -4623,6 +4639,14 @@ exit_skip:
 	mi_cfg->layer_fod_unlock_success = false;
 	mi_cfg->sysfs_fod_unlock_success = false;
 	fm_stat.idle_status = false;
+
+#ifdef CONFIG_D8G_SERVICE
+	oplus_panel_status = 2; // DISPLAY_POWER_ON
+	if (oprofile_last < 0) 
+		oprofile_last = oprofile;
+	oprofile = oprofile_last; // set oprofile to last profile setted
+	set_amoled_display();
+#endif
 
 exit:
 	mutex_unlock(&panel->panel_lock);
@@ -5039,6 +5063,14 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	mi_cfg->into_aod_pending = false;
 	fm_stat.idle_status = false;
 
+#ifdef CONFIG_D8G_SERVICE
+	oplus_panel_status = 2; // DISPLAY_POWER_ON
+	if (oprofile_last < 0) 
+		oprofile_last = oprofile;
+	oprofile = oprofile_last; // set oprofile to last profile setted
+	set_amoled_display();
+#endif
+
 	mutex_unlock(&panel->panel_lock);
 	display_utc_time_marker("DSI_CMD_SET_ON");
 
@@ -5249,6 +5281,12 @@ int dsi_panel_disable(struct dsi_panel *panel)
 	mi_cfg->sysfs_fod_unlock_success = false;
 	if (mi_cfg->dc_type)
 		mi_cfg->dc_enable = false;
+
+#ifdef CONFIG_D8G_SERVICE
+	oplus_panel_status = 0; // DISPLAY_POWER_OFF
+	oprofile_last = oprofile; // Get latest profile
+	oprofile = 4; // set oprofile to battery when display off
+#endif
 
 	mutex_unlock(&panel->panel_lock);
 	display_utc_time_marker("DSI_CMD_SET_OFF");
